be9f077c7180f949bf2eca429b3b2348d44bc930
Gerald <ggaereminck@yahoo.com>
2021-10-18 15:13:39 -0400

    commit tree 77859426f20220297be92b660b60f78e6b282d09

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :sideone, :sidetwo, :sidethree
+
+  def initialize(sideone, sidetwo, sidethree)
+    @sideone = sideone
+    @sidetwo = sidetwo
+    @sidethree = sidethree
+  end
+
+  def kind
+    validate_triangle
+    if sideone == sidetwo && sidetwo == sidethree
+      :equilateral
+    elsif sideone == sidetwo || sidetwo == sidethree || sideone == sidethree
+      :isosceles
+    else :scalene
+    end
+  end
+
+  def over_zero?
+    [sideone, sidetwo, sidethree].all?(&:positive?)
+  end
+
+  def inequality?
+    sideone + sidetwo > sidethree && sideone + sidethree > sidetwo && sidetwo + sidethree > sideone
+  end
+
+  def validate_triangle
+    raise TriangleError unless over_zero? && inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
